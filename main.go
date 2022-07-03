5d36624df42c05cc476975a684e27f282dc0cb20
AvigdorVIK <kanynnikovvik19@gmail.com>
2022-07-03 12:58:00 +0300

    if

--- main.go
+++ main.go
@@ -6,13 +6,29 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("privet is snapchat"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("storinka vidobrajenya"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("storinka stvorenya novoi statti"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	log.Println("zapusk servera na port ...")
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("zapusk servera na port :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
